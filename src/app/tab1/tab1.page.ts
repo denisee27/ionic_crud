352775239b5c6183ed5c600b418a7e6bf91829ad
Denis Aldianto <denisaldian11@gmail.com>
2024-03-26 16:39:05 +0700

    Done CRUD

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,9 +1,13 @@
-import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { LoadingService } from '../services/loading.service';
 import { HttpService } from '../services/http.service';
 import { ToastrService } from 'ngx-toastr';
 import { PageQueryService } from '../services/page-query.service';
 import { ModalController } from '@ionic/angular';
+import { OverlayEventDetail } from '@ionic/core/components';
+import { IonModal } from '@ionic/angular/common';
+import { FormBuilder, FormControl, FormGroup, NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'app-tab1',
@@ -15,14 +19,20 @@ export class Tab1Page implements OnInit, OnDestroy {
   constructor(
     public pageQuery: PageQueryService,
     public loading: LoadingService,
+    private el: ElementRef,
     private http: HttpService,
+    private fb: FormBuilder,
     private modalController: ModalController
   ) { }
   @ViewChild('modal') Modal: any;
+  @ViewChild('update') update: IonModal;
+  @ViewChild('theForm') theForm: NgForm | undefined;
 
   private apiPath = 'companies';
   data: any = {};
+  formGroup = this.fb.group({});
   deleteModal = false;
+  updateModal = false;
 
   deleteDialog: any = {
     ids: [],
@@ -44,6 +54,43 @@ export class Tab1Page implements OnInit, OnDestroy {
         });
     }
 }
+  updateDialog: any = {
+    ids: [],
+    data:[],
+    modal: null,
+    formGroup: new FormGroup({}), // Menambahkan FormGroup di dalam objek updateDialog
+    show: (ids: Array<any>) => {
+      this.updateDialog.ids = Array.isArray(ids) ? ids : [ids];
+      var updateData = this.data.data.filter((e:any) => e.id == ids);      
+      Object.keys(this.formGroup.controls).forEach(key => {
+        this.formGroup.removeControl(key);
+      });
+      Object.keys(updateData[0]).forEach((key) => {
+        this.formGroup.addControl(key, new FormControl(updateData[0][key]));
+      });
+      this.updateModal = true;
+    },
+    close:() => {
+      this.updateModal = false
+    },
+    submit: () => {
+        this.http.Delete(this.apiPath + '/delete', this.updateDialog.ids).then((r: any) => {
+            if (r.success) {
+                this.data = r?.response?.result || {};
+                this.updateModal = false
+                this.getData()
+              }
+        });
+    }
+}
+
+cancel() {
+  this.update.dismiss(null, 'cancel');
+}
+
+confirm() {
+  this.update.dismiss();
+}
 
   getData(): void {
     const query: any = this.pageQuery.query.getValue() || {};
@@ -55,6 +102,32 @@ export class Tab1Page implements OnInit, OnDestroy {
       }
     });
   }
+
+  submitForm(form: any): void {
+    if (!form.valid) {
+        this.el.nativeElement.querySelectorAll('[formcontrolname].ng-invalid')?.[0]?.focus();
+        return;
+    }
+
+    let fdata = new FormData();
+    form.value.status = Number(form.value.status);
+    fdata.append('data', JSON.stringify(form.value));
+    form.disable();
+    this.http.PostFile(this.apiPath + '/update', fdata).then((r: any) => {
+        form.enable();
+        if (r.success) {
+          this.getData()
+          this.updateModal = false
+        } else {
+            if (r.response && r.response.wrong) {
+                Object.keys(r.response.wrong).forEach((key) => {
+                    form.get(key)?.setErrors({ serverError: r.response.wrong[key][0] });
+                    this.el.nativeElement.querySelectorAll('[formcontrolname="' + key + '"]')?.[0]?.focus();
+                });
+            }
+        }
+    });
+}
   
   ionViewDidEnter(){
   this.getData()
@@ -67,5 +140,7 @@ export class Tab1Page implements OnInit, OnDestroy {
   ngOnDestroy(): void {
     this.pageQuery.destroy();
 }
+
+
 
 }
